f0c8245979cc5dde75a985fbcec95a6667b010a5
basting <bastin.gomez@gmail.com>
2015-11-25 20:12:25 -0500

    Updates

--- src/com/bighi/algos/week6/median/MaxHeap.java
+++ src/com/bighi/algos/week6/median/MaxHeap.java
@@ -1,5 +1,21 @@
 package com.bighi.algos.week6.median;
 
-public class MaxHeap {
-	
+import java.util.Comparator;
+
+public class MaxHeap extends Heap<Integer> {
+
+	public MaxHeap() {
+		this(new MaxHeapComparator());
+	}
+
+	public MaxHeap(Comparator<Integer> comparator) {
+		super(comparator);
+	}
+
+	static class MaxHeapComparator implements Comparator<Integer> {
+		public int compare(Integer o1, Integer o2) {
+			return o1.compareTo(o2);
+		}
+	}
+
 }
--- src/com/bighi/algos/week6/median/MinHeap.java
+++ src/com/bighi/algos/week6/median/MinHeap.java
@@ -1,19 +1,20 @@
 package com.bighi.algos.week6.median;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 
-public class MinHeap<T> {
-	private ArrayList<T> elems = new ArrayList<>();
-	
-	public void insert(int e) {
-		
+
+public class MinHeap extends Heap<Integer> {
+	public MinHeap() {
+		this(new MinHeapComparator());
 	}
-	
-	public T extractMin() {
-		if(elems.size() == 0) {
-			return null;
+
+	public MinHeap(Comparator<Integer> comparator) {
+		super(comparator);
+	}
+
+	static class MinHeapComparator implements Comparator<Integer> {
+		public int compare(Integer o1, Integer o2) {
+			return o1.compareTo(o2);
 		}
-		
-		return null;
 	}
 }
